d9756209d9aee638dc73db435e1c3fe66dcff939
AKASH KUMAR JAISWAL <52071795+akku9621@users.noreply.github.com>
2025-09-23 18:02:24 +0530

    Revert "added env file"

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.BACKEND_URL}/api/:path*`, // ✅ backend
+        destination: "https://chashma-backend.vercel.app/api/:path*", // ✅ backend
       },
     ];
   },
